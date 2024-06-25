2acaa03109adffcd3eb3d8f6910054ab4bf170e5
josvillegas <josvillegas97@gmail.com>
2024-06-25 12:13:11 -0500

    init project

---
+++ src/data/navData.js
@@ -0,0 +1,16 @@
+const navData = [
+  {
+    name: "Inicio",
+    path: "/",
+  },
+  {
+    name: "Proyectos",
+    path: "/proyectos/",
+  },
+  {
+    name: "Experiencia",
+    path: "/experiencia/",
+  },
+];
+
+export default navData;
