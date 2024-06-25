3b131ea942fd0bd4611bd9af9ec8d2ca864a7257
josvillegas <josvillegas97@gmail.com>
2024-06-25 16:28:27 -0500

    update details in deploy, img, content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
   devToolbar: {
-    enabled: false,
+    enabled: false
   },
+  adapter: vercel()
 });
